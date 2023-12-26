73e944fd9e472a14f2bbc51e5a2a87c38754c613
ayushjha119 <ayush.jha.it23@heritageit.edu.in>
2023-12-26 17:59:03 +0530

    Navbar Created

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
 import styles from "./authLinks.module.css";
 import React from "react";
 
 const AuthLinks = () => {
-  return <div className={styles.container}>AuthLinks</div>;
+  const status = "notauthenticated";
+
+  return (
+    <>
+      {status === "notauthenticated" ? (
+        <Link href="/login">Login</Link>
+      ) : (
+        <>
+          <Link href="/write">Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLinks;
